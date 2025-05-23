683d67d1d55e0dafb6896693a12e20bc23181d5e
jhon <zhaohanzheng@yeah.net>
2025-05-24 00:36:48 +0800

    finished the d in the round 1025

--- 1025/b.cpp
+++ 1025/b.cpp
@@ -8,15 +8,28 @@ using namespace std;
 #endif
 typedef long long ll;
 
+int work(int a, int b) {
+    int res = 0;
+    while(a!=1) a=(a+1)/2,res++;
+    while(b!=1) b=(b+1)/2,res++;
+    return res;
+}
+
+// 3322 23 13 12 11
+
 void solve() {
-    cout << "hello world" << endl;
+    int n,m,a,b;
+    cin >> n >> m >> a >> b;
+    a = min(n-a+1,a);
+    b = min(m-b+1,b);
+    cout << min(work(a,m),work(n,b))+1 << endl;
 }
 
 int main() {
     INPUT("in.txt");
     cout << "hello world" << endl;
-    //ios::sync_with_stdio(false);
-    //cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
     int tc;
      cin >> tc; 
      while(tc--) solve(); 
