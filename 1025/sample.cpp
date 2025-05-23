f262bc5a3b578416e1575216b24f080a87044417
jhon <zhaohanzheng@yeah.net>
2025-05-23 15:39:33 +0800

    使用d.cpp作为演示

---
+++ 1025/sample.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#ifdef LOCAL
+#define INPUT(from) freopen(from, "r", stdin)
+#else
+#define INPUT(from) ((void)0)
+#endif
+typedef long long ll;
+
+void solve() {
+
+}
+
+int main() {
+    INPUT("in.txt");
+    string s;
+    cin >> s;
+    cout << s << endl;
+    
+}
