f8e555a0c3d8d216a3de44212a3f2832e4913f31
jhon <zhaohanzheng@yeah.net>
2025-05-24 11:31:34 +0800

    这次真的把1025 d题过了

--- 1025/d.cpp
+++ 1025/d.cpp
@@ -7,64 +7,66 @@ using namespace std;
 #define INPUT(from) ((void)0)
 #endif
 typedef long long ll;
+const int inf  = 2e9+1;
 
-void dij(int n, vector<int>v[] , vector<array<int, 2>>&dis) {
+void bfs(int n, vector<int> v[], vector<array<int, 2>>& dis) {
+    dis.assign(n + 1, {inf,inf});
     dis[1][0] = 0;
-    queue<pair <int,int> >q;
-    q.push({1,0});
-    while(q.size()) {
-        auto [x,d] = q.front();
+    queue<pair<int, int>> q;
+    q.push({1, 0});
+    while (!q.empty()) {
+        auto [x, p] = q.front();
         q.pop();
-        for(auto &son : v[x]) {
-            auto now = d+1;
-            if(now < dis[son][d%2]) {
-                q.push({son,now});
-                dis[son][d%2] = now;
+        for (auto& son : v[x]) {
+            int new_p = 1 - p; // 切换奇偶性
+            if (dis[son][new_p] > dis[x][p] + 1) {
+                dis[son][new_p] = dis[x][p] + 1;
+                q.push({son, new_p});
             }
         }
     }
-    return ;
 }
 
 void solve() {
-    int n,m,l;
+    int n, m, l;
     cin >> n >> m >> l;
-//    cout << n << " " << m << " " << l << endl;
-    vector<int> a(l,0);
-    ll odds = 0x3f3f3f3f+1;
-    ll sum = 0;
-    for(auto &i:a) cin >> i,sum+=i , i%2?odds = min(odds,(ll)i) :1;
-    vector<int> v[n+1];
-    for(int i = 0 ; i < m ; i++ ) {
-        int x,y;
+    vector<int> a(l);
+    int sum = 0, min_odd = inf ;
+    for (auto& i : a) {
+        cin >> i;
+        sum += i;
+        if (i % 2) min_odd = min(min_odd, i);
+    }
+    bool has_odd = (min_odd != inf );
+
+    vector<int> v[n + 1];
+    for (int i = 0; i < m; ++i) {
+        int x, y;
         cin >> x >> y;
         v[x].push_back(y);
-        v[y].push_back(x); 
+        v[y].push_back(x);
     }
-    vector<array<int, 2>> dis(n+1,{0x3fffffff,0x3fffffff})  ;
-    dij(n, v, dis);
-    
-    //for(int i = 1 ; i <= n ; i++) {int x=dis[i][0] ; cout << x <<  " ";}
-    cout << endl;
-    //for(int i = 1 ; i <= n ; i++) {int x=dis[i][1] ; cout << x <<  " ";}
-    cout << endl;
-    
-    for(int i = 1 ; i <= n ; i++) {
-        auto greet = [&](int p) { 
-            if(dis[i][p] > 2e5) return 0;
-            if(dis[i][p]%2 == sum%2) {
-                if(sum>=dis[i][p]) return 1;
-                else return 0;
-            } else {
-                if(odds < 1e4 && sum-odds >=dis[i][p]) return 1;
-                else return 0;
-            }
-        };
-        cout << max(greet(0),greet(1));
-    }
-    cout << endl;
-} 
 
+    vector<array<int, 2>> dis;
+    bfs(n, v, dis);
+
+    for (int i = 1; i <= n; ++i) {
+        bool found = false;
+        for (int p : {0, 1}) {
+            auto s = sum;
+            if (p != sum % 2) {
+                if (!has_odd) continue;
+                s -= min_odd;
+            }
+            if (s >= 0 && dis[i][p] <= s) {
+                found = true;
+                break;
+            }
+        }
+        cout << found;
+    }
+    cout << endl;
+}
 int main() {
     INPUT("in.txt");
     ios::sync_with_stdio(false);
